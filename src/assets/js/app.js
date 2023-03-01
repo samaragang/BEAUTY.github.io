82a1c00c203b5b2c5fd7b62b85a5405b9b34be60
Dima Burii <dmitr.burii.2016@gmail.com>
2023-03-01 18:20:13 +0300

    gh-pages

--- src/assets/js/app.js
+++
@@ -1,9 +0,0 @@
-console.log('file 1');
-(function () {
-    const burger = document.querySelector('.burger');
-    const navbar = document.querySelector('.header__navbar');
-    burger.addEventListener('click', () => {
-        burger.classList.toggle('burger_active');
-        navbar.classList.toggle('navbar_active');
-    });
-}());
